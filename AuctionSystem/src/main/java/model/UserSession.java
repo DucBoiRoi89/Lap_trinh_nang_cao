cbab3a179d984c9fb5dfb964406c46a2585bd17a
Trương Anh Đức <anhduc931769@gmail.com>
2026-06-03 10:16:22 +0700

    Delete AuctionSystem directory

--- AuctionSystem/src/main/java/model/UserSession.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class UserSession {
-    private static User loggedInUser;
-    public static void setLoggedInUser(User user) { loggedInUser = user; }
-    public static User getLoggedInUser() { return loggedInUser; }
-    public static void cleanUserSession() { loggedInUser = null; }
-}
