d0da0b421b56c5b27e029aa9c2f2aa13692d89d6
Trương Anh Đức <anhduc931769@gmail.com>
2026-07-22 21:16:57 +0700

    Hoan thanh bai 10

---
+++ TruongAnhDuc_25021741_Week2/Bai10/src/CentralHub.java
@@ -0,0 +1,8 @@
+public class CentralHub{
+    public CentralHub(){
+        
+    }
+    public static void registerDevice(SmartLight light){
+        System.out.println("[HUB] Dang ket noi voi thiet bi: " + light.getName());
+    }
+}
---
+++ TruongAnhDuc_25021741_Week2/Bai10/src/SmartLight.java
@@ -0,0 +1,36 @@
+public class SmartLight{
+    private String id;
+    private String name;
+    private int brightness;
+    public SmartLight(String id, String name, int brightness){
+        this.id = id;
+        this.name = name;
+        this.brightness = brightness;
+    }
+    public SmartLight(String id, String name){
+        this.id = id;
+        this.name = name;
+        this.brightness = 50;
+    }
+    public void setBrightness(int brightness){
+        this.brightness = brightness;
+    }
+    public void setBrightness(String preset){
+        if (preset.equals("MAX")){
+            this.setBrightness(100);
+        } else if (preset.equals("MIN")){
+            this.setBrightness(10);
+        } else if (preset.equals("ECO")){
+            this.setBrightness(30);
+        }
+    }
+    public void connectToHub(CentralHub hub){
+        hub.registerDevice(this);
+    }
+    public String getName(){
+        return this.name;
+    }
+    public int getBrightness(){
+        return this.brightness;
+    }
+}
