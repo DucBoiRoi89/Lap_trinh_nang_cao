02282b6505a0bf6a4764d38c93fc4e7e904881c7
Trương Anh Đức <anhduc931769@gmail.com>
2026-07-22 21:18:20 +0700

    Hoan thanh bai 10

--- TruongAnhDuc_25021741_Week2/Bai10/src/CentralHub.java
+++ TruongAnhDuc_25021741_Week2/Bai10/src/CentralHub.java
@@ -1,6 +1,5 @@
 public class CentralHub{
-    public CentralHub(){
-        
+    public CentralHub(){   
     }
     public static void registerDevice(SmartLight light){
         System.out.println("[HUB] Dang ket noi voi thiet bi: " + light.getName());
