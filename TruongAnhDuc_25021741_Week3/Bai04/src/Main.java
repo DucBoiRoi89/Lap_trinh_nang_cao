6edaa968b55d72f4f0c314690331fc18dcd79da8
Trương Anh Đức <anhduc931769@gmail.com>
2026-07-27 10:32:32 +0700

    Hoan thanh bai 4

---
+++ TruongAnhDuc_25021741_Week3/Bai04/src/Main.java
@@ -0,0 +1,11 @@
+class Main{
+    public static void main(String[] args){
+        Animal a = new Dog();
+        if (a instanceof Cat){
+            Cat c = (Cat) a;
+            c.makeSound();
+        } else{
+            System.out.println("Day khong phai la Meo!");
+        }
+    }
+}
